eec2b0c55d99956dfd1289707c3ce1f7ad3e66e1
Trakatranks <alexandre.ferlet@hotmail.fr>
2023-01-28 00:02:54 +0100

    fix: changed pdf default file name
    💖

--- src/utils/generatePdf.ts
+++ src/utils/generatePdf.ts
@@ -7,10 +7,13 @@ import Report from './Report'
 import { MedicalFile, Settings } from '../types'
 
 export async function generatePdf(file: MedicalFile, settings: Settings) {
+
+  const fileName = "Compte_Rendu_Consultation_"+file.patientFirstName+'_'+file.patientLastName
+
   const path = await save({
     title: 'Enregistrer le document',
-    defaultPath: './document.pdf',
-    filters: [{ name: 'document.pdf', extensions: ['pdf'] }],
+    defaultPath: './' + fileName + '.pdf',
+    filters: [{ name: fileName + '.pdf', extensions: ['pdf'] }],
   })
 
   const doc = await pdf(Report({ file, settings })).toBlob()
